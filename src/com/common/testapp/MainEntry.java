bed1a11afdc02c9b5e0e1110c58f60666366b3a5
Prolay Sarkar <prolay@ymail.com>
2014-08-09 15:56:16 +0530

    swing initial commit

--- src/com/common/testapp/MainEntry.java
+++ src/com/common/testapp/MainEntry.java
@@ -1,10 +1,36 @@
 package com.common.testapp;
 
-public class MainEntry {
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class MainEntry implements Runnable {
+
+	@Override
+    public void run() {
+        // Create the window
+        JFrame f = new JFrame("Hello, World!");
+        // Sets the behavior for when the window is closed
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Add a layout manager so that the button is not placed on top of the label
+        f.setLayout(new FlowLayout());
+        // Add a label and a button
+        f.add(new JLabel("Hello, world!"));
+        f.add(new JButton("Press me!"));
+        // Arrange the components inside the window
+        f.pack();
+        // By default, the window is not visible. Make it visible.
+        f.setVisible(true);
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		MainEntry objNew = new MainEntry();
+		SwingUtilities.invokeLater(objNew);
 	}
 
 }
